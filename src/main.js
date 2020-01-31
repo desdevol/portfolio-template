9a33ea3b853d623e8ca30b6782521447e125d9c0
Lee Yong Wei <ywlee9999@hotmail.com>
2020-01-31 23:05:21 +0800

    init quasar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import "./quasar";
+import './quasar'
+import './quasar'
+import './quasar'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+    render: h => h(App)
+}).$mount("#app");
---
+++ src/quasar.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+import 'quasar/dist/quasar.css'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+    config: {},
+    components: { /* not needed if importStrategy is not 'manual' */ },
+    directives: { /* not needed if importStrategy is not 'manual' */ },
+    plugins: {
+    }
+})
