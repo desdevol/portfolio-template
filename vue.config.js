51186a0eb26bbc33c32dca3807eb0a9780341d26
Lee Yong Wei <ywlee9999@hotmail.com>
2020-02-06 02:20:34 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 module.exports = {
     pluginOptions: {
         quasar: {
-            importStrategy: 'kebab',
+            importStrategy: "kebab",
             rtlSupport: false
         }
     },
-    transpileDependencies: [
-        'quasar'
-    ]
-}
+    transpileDependencies: ["quasar"],
+    chainWebpack: config => {
+        // Pug Loader
+        config.module
+            .rule("pug")
+            .test(/\.pug$/)
+            .use("pug-plain-loader")
+            .loader("pug-plain-loader")
+            .end();
+    }
+};
