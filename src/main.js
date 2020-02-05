c5eeaaafa9a5bc9c1df8c33bcf060213654bdf2f
Lee Yong Wei <ywlee9999@hotmail.com>
2020-02-06 02:19:47 +0800

    import css to main js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./quasar";
-import './quasar'
-import './quasar'
-import './quasar'
+import "./assets/css/style.css";
 
 Vue.config.productionTip = false;
 
