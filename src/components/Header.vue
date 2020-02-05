18941ec8b49ceef3279d6e448eec19a31bf6eac3
Lee Yong Wei <ywlee9999@hotmail.com>
2020-02-06 02:20:47 +0800

    app init

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+q-header.bg-primary.text-white(bordered)
+    q-toolbar.bg-primary.text-whiterounded-borders
+        q-btn(flat, label='Homepage')
+        q-space
+        q-tabs(v-model='tab', shrink)
+            q-tab(name='tab1', label='Tab 1')
+            q-tab(name='tab2', label='Tab 2')
+            q-tab(name='tab3', label='Tab 3')
+
+</template>
+
+<script>
+export default {
+    props: {},
+    components: {},
+    mixins: [],
+    data: function () {
+        return {
+            tab: ''
+        };
+    },
+    watch: {},
+    computed: {},
+    created: function () { },
+    beforeDestroy: function () { },
+    mounted: function () { },
+    methods: {},
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    q-page.flex.flex-center(dark)
+        img(
+            alt="Quasar logo"
+            src="../assets/logo.png"
+        )
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+    name: 'HelloWorld'
+}
+</script>
